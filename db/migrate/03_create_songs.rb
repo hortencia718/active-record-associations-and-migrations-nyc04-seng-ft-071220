34f17d252a7f6b749154932de3fecde9c515bcd5
hortencia c <hortenciaecisneros@gmail.com>
2020-08-13 19:39:45 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,10 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+  def change
+    create_table
+    :songs do |t|
+      t.string :name 
+      t.Integer :artist_id t.Integer :genre_id
+  
+   end
+  end 
 end
